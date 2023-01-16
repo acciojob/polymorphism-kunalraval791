cb3fe747e453c2ddedb378db300d425eb1cdc582
kunalraval791 <121260919+kunalraval791@users.noreply.github.com>
2023-01-16 23:15:42 +0530

    Solution 16

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ package com.driver;
 public class Main {
     public void main(String[] args) {
         Product p = new Product();
-        int prod1 = p.product(2, 3);
-        int prod2 = p.product(2, 3, 4);
-        double prod3 = p.product(5, 6);
+        p.product(2, 3);
+        p.product(2, 3, 4);
+        p.product(5.02, 6.01);
     }
     public class Product{
         public int product(int x, int y){
